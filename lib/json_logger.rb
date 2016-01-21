451c9faf7e7632c254553f6ba1b4c3055f037acb
DmitryKK <dimak33@ya.ru>
2016-01-21 16:47:49 +0300

    Added json formatter

--- lib/json_logger.rb
+++ lib/json_logger.rb
@@ -1,5 +1,29 @@
 require "json_logger/version"
+require "json"
+require "logger"
 
 module JsonLogger
-  # Your code goes here...
+  class Formatter < Logger::Formatter
+    def call(severity, timestamp, progname, message)
+      default_hash = {
+        type: severity,
+        time: timestamp,
+        user_id: Thread.current[:user].try(:id)
+      }
+
+      case message
+      when Hash
+        default_hash.merge(message)
+      when String
+        default_hash.merge(message: message)
+      when Exception
+        default_hash.merge(kind: :exception,
+                           message: message.message,
+                           class: message.class.name,
+                           backtrace: message.backtrace || [])
+      else
+        default_hash.merge(message: message.inspect)
+      end.to_json + "\n"
+    end
+  end
 end
