1ffb2e1192a7e9de4439ec3ba3cad346b12048d6
DmitryKK <dimak33@ya.ru>
2016-01-28 13:50:07 +0300

    Updated logger

--- lib/json_logger/formatter.rb
+++ lib/json_logger/formatter.rb
@@ -1,11 +1,10 @@
 module JsonLogger
   class Formatter < Logger::Formatter
     def call(severity, timestamp, progname, message)
-      default_hash = {
-        type: severity,
-        time: timestamp,
-        user_id: Thread.current[:user].try(:id)
-      }
+      default_hash = { type: severity, time: timestamp }
+
+      default_hash.merge!(user_id: user.id) if user
+      default_hash.merge!(url: request.original_url, params: request.path_parameters) if request
 
       case message
       when Hash
@@ -20,6 +19,16 @@ module JsonLogger
       else
         default_hash.merge(message: message.inspect)
       end.to_json + "\n"
+    end
+
+    private
+
+    def request
+      Thread.current[:request]
+    end
+
+    def user
+      Thread.current[:user]
     end
   end
 end
